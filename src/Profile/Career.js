c5e2c5f36ff15d7c2cc3b0fa46fccdb818ee1f6f
wlsgus5932 <wlsgus1149@naver.com>
2023-01-16 13:03:28 +0900

    dd

--- src/Profile/Career.js
+++ src/Profile/Career.js
@@ -4,6 +4,7 @@ import '../css/Career.css';
 const Career = () => {
   return (
     <div className="Career">
+      <div>준비중s</div>
         <section className='section1'>dd</section>
         <section className='section2'>dd</section>
         <section className='section3'>dd</section>
