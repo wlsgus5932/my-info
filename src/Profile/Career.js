674305db67cb10027bf639156b75a28a05255267
wlsgus5932 <wlsgus1149@naver.com>
2023-01-18 00:22:13 +0900

    dd

--- src/Profile/Career.js
+++ src/Profile/Career.js
@@ -6,13 +6,15 @@ import { faSquareInstagram, faGithub } from "@fortawesome/free-brands-svg-icons"
 const Career = () => {
   return (
     <div className="Career">
-        <section className='section1'>준비중<br/>(주) 미디어그룹 사람과 숲 2021-08-12 ~ </section>
-        <section className='section2'>준비중<br/>저는요
-        <FontAwesomeIcon icon={faSquareInstagram} />
-        <FontAwesomeIcon icon={faGithub} />
+        <section className='section1'>준비중<br/><br/> 현재 웹 사용기술: <br/> <br/> 클라우드: AWS ec2 <br/> 웹서버: NGINX <br/> 
+        프론트엔드: React.js <br/> 백엔드: SpringBoot <br/> 데이터베이스: Postgresql </section>
+        <section className='section2'>준비중<br/><br/>(주) 미디어그룹 사람과 숲 2021-08-12 ~ now
         
         </section>
-        <section className='section3'>준비중<br/>목표</section>
+        <section className='section3'>준비중<br/><br/> 저는요// 목표 
+          <FontAwesomeIcon icon={faSquareInstagram} />
+          <FontAwesomeIcon icon={faGithub} />
+        몰라</section>
     </div>
   );
 }
