ba358729ca423a01dbdc2e53c4c05d207a95936a
wlsgus5932 <wlsgus1149@naver.com>
2023-01-21 03:17:10 +0900

    d

--- src/Profile/Career.js
+++ src/Profile/Career.js
@@ -9,13 +9,13 @@ const Career = () => {
         <section className='section1'>
         <span className='career-remark-text'>{'/* 개발자는 미래를 개척해나가는 직업 */'}</span>
         </section>
-        {/* <section className='section2'>준비중<br/><br/>(주) 미디어그룹 사람과 숲 2021-08-12 ~ now
+        <section className='section2'>준비중<br/><br/>(주) 미디어그룹 사람과 숲 2021-08-12 ~ now
         
         </section>
         <section className='section3'>준비중<br/><br/> 저는요// 목표 
           <FontAwesomeIcon icon={faSquareInstagram} />
           <FontAwesomeIcon icon={faGithub} />
-        몰라</section> */}
+        몰라</section>
     </div> 
   );
 }
