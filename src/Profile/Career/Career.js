97919e1806ff50dae756051d4b4752bc68c75bc5
wlsgus5932 <wlsgus1149@naver.com>
2023-08-22 22:45:24 +0900

    test

--- src/Profile/Career/Career.js
+++ src/Profile/Career/Career.js
@@ -6,6 +6,7 @@ import { faSquareInstagram, faGithub } from "@fortawesome/free-brands-svg-icons"
 const Career = () => {
   return (
     <div className="Career">
+      <div>test</div>
       {/* <h1 className='Career-text'>test</h1> */}
         {/* <section className='section1'><br/>준비중<br/><br/> 클라우드: aws ec2<br/> 웹서버: NGINX <br/>프론트엔드: React <br/> 백엔드: SpringBoot <br/> 데이터베이스: Postgresql </section>
         <section className='section2'><br/>준비중<br/><br/>(주) 미디어그룹 사람과 숲 2021-08-12 ~ 2023-06-16
